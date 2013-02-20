37f37631191859ad181c8430a4f701db027b2cd4
Andrew Noonan <andrew@gnipcentral.com>
2013-02-19 22:46:12 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rosterite::Application.config.secret_token = '47d8fa4bb6e182a9ab78601ce016c52b048c344a0d9e4c78a26d2b9c549a3774078d4676cce0e63286a1710dc3718d8e199afddf30958d285150deb0011217dc'
